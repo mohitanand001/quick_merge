279b6a3b2757469ace1243457f760f36488b832d
farziengineer <anand.mohit001@gmail.com>
2019-10-21 18:04:54 +0530

    Added test for heap.py

---
+++ tests/test_heap.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.heap import Heap
+
+@pytest.mark.parametrize("test_input,expected", [ ([8, 1, 2, 4], 8),
+                                                  ([1, 2, 5, 3], 5),
+                                                  ([5], 5)] ) 
+def test_get_top_element(test_input, expected):
+    
+    heap_obj = Heap()
+    heap_obj.build_heap(test_input)
+    assert heap_obj.get_top_element() == expected
