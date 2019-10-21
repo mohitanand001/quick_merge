f722cedbc4c80259bfac970764455d049987525d
farziengineer <anand.mohit001@gmail.com>
2019-10-21 23:53:38 +0530

    Added tests for getting top element.

--- tests/test_heap.py
+++ tests/test_heap.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import pytest
 
 from src.heap import Heap
@@ -6,7 +10,7 @@ from src.heap import Heap
                                                   ([1, 2, 5, 3], 5),
                                                   ([5], 5)] ) 
 def test_get_top_element(test_input, expected):
-    
     heap_obj = Heap()
     heap_obj.build_heap(test_input)
-    assert heap_obj.get_top_element() == expected
+    assert heap_obj.get_top_element() == 1
+
