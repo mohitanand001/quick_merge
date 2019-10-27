cd12b08e121b5c39ac1ec10a5581c53496f9b36c
farziengineer <anand.mohit001@gmail.com>
2019-10-27 18:29:48 +0530

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+
+from setuptools import find_packages, setup
+
+setup(
+    name="quick_merge",
+    version="0.0.1",
+    description="Quickly merge files in bulk.",
+    maintainer="Mohit Anand",
+    maintainer_email="anand.mohit001@gmail.com",
+    url="https://github.com/farziengineer/quick_merge",
+    platforms="any",
+    packages=find_packages(),
+    keywords=["pandas",],
+    install_requires=["pandas"],
+)
