36d35b722775554cdd67a65ab8dfd5b3ace2806e
farziengineer <anand.mohit001@gmail.com>
2019-11-06 10:01:01 +0530

    Added sphinx doc to bulk_merge.

--- quick_merge/bulk_merge.py
+++ quick_merge/bulk_merge.py
@@ -8,63 +8,92 @@ from quick_merge.heap import Heap
 
 
 class QuickMerge:
-    def __init__(self, container, key, custom_comp=None):
-        self.container = container
-        self.custom_comp = custom_comp
+    """Performs full outer join(merge) of large number of
+    dataframes in an optimized manner. The algorithm capitalizes
+    on the fact that full outer joins(merge) is both commutative
+    and associative.
+    :param container: Container of dataframes
+    :type container: List[pandas.core.frame.DataFrame]
+    :param merge_key: The key to be indexed by which we have to merge the dataframes.
+    :type merge_key: str, optional
+    """
+    def __init__(self, container, key=None):
+        self.container = container.copy()
         self.merge_key = key
 
+    def index_dataframes_util(self):
+        """Utility function for indexing the dataframe by
+        `key`.
+        :raises KeyError: Raises a KeyError when trying to set_index on a column that does
+        not exist. 
+        """
+        if self.merge_key is not None:
+            self.container = [df.set_index(self.merge_key, inplace=True) for df in self.container]
+
+
     def quick_merge_util(self, hp_obj):
-        # for _ in range(4):
-        #     print(hp_obj.pop_top_element(),)
-        # print(hp_obj.heap_sort())
-        # cnt = 0
+        """Maintains a heap of dataframes, constantly
+        merges(outer) two smallest dataframes and keeps
+        on adding it to the heap.
+        :param hp_obj: object of :class:`Heap`
+        :return: A pandas Dataframe having the merged(outer) data.
+        :rtype: pandas.core.frame.DataFrame
+         """
+
         while hp_obj.get_size() > 1:
             curr_df_1 = hp_obj.pop_top_element()
             curr_df_2 = hp_obj.pop_top_element()
-
-            print(curr_df_1.size, curr_df_2.size, hp_obj.get_size())
-
-            merged_df = pd.merge(curr_df_1, curr_df_2, on=self.merge_key, how="outer")
+            merged_df = pd.merge(curr_df_1,
+                                 curr_df_2,
+                                 how="outer",
+                                 left_index=True,
+                                 right_index=True)
             hp_obj.insert_node(merged_df)
 
         return hp_obj.pop_top_element()
 
     def quick_merge_driver(self):
-        heap_obj = Heap(self.container, self.custom_comp)
+        """Driver function for creating `:class:Heap` object and 
+        setting index on the list of dataframes. The `:class:Heap` is
+        balanced in such a way that dataframe with minimum size is 
+        at the top. Also calls `quick_merge_util` which does the merging(outer) 
+        and returns the final merged dataframe.
+        :return: A pandas Dataframe having the merged(outer) data.
+        :rtype: pandas.core.frame.DataFrame
+        """
+        heap_obj = Heap(self.container, lambda x, y: x.size < y.size)
         heap_obj.build_heap()
-        # print(self.container)
-
+        self.index_dataframes_util()
         return self.quick_merge_util(heap_obj)
 
 
-def quick_merge(dataframes=None, merge_key=None, ):
-    q_merge_obj = QuickMerge(dataframes, merge_key, lambda x, y: x.size < y.size)
+def quick_merge(dataframes=None, merge_key=None):
+    q_merge_obj = QuickMerge(dataframes, merge_key, )
     res = q_merge_obj.quick_merge_driver()
     return res
 
 if __name__ == "__main__":
-    base_dir = '/Users/mohitanand/quick_merge/files'
+    base_dir = '/Users/mohitanand/quick_merge/temp_files'
     files = os.listdir(base_dir)
     files.sort()
-    print('sorted')
+
     dfs = [pd.read_csv(os.path.join(base_dir, file_), index_col='A') for file_ in files]
-    print('read')
-    # df_ = quick_merge(dfs, 'A', lambda x, y: x.size < y.size)
-    # print(df_.shape)
-    df_final = reduce(lambda left,right: pd.merge(left,right, on='A', left_index=True, right_index=True, how='outer'), dfs)
+
+    df_ = quick_merge(dfs,)
+
+    dfs_cop = dfs.copy()
+    df_final = dfs_cop[0]
+    for df in dfs_cop[1:]:
+        df_final = pd.merge(df_final, df,  how='outer', left_index=True, right_index=True,)
 
 
-    # df_1 = pd.DataFrame(
-    #     np.random.randint(0, 100, size=(100, 6)), columns=list("ABCDEF")
-    # )
-    # df_2 = pd.DataFrame(
-    #     np.random.randint(0, 100, size=(100, 9)), columns=list("ABCDEFGHI")
-    # )
-    # df_3 = pd.DataFrame(np.random.randint(0, 100, size=(100, 3)), columns=list("ABC"))
+    df_ = df_[sorted(df_.columns)]
+    df_final = df_final[sorted(df_final.columns)]
 
-    # A = [df_1, df_2, df_3]
-    # q_merge_obj = QuickMerge(A, "A", lambda x, y: x.size < y.size)
-    # print(q_merge_obj.quick_merge_driver().head())
+ 
+    df_.fillna(0, inplace=True)
+    df_final.fillna(0, inplace=True)
 
-    # q_o = QuickMerge([3, 2, 1, 5], lambda x,y: x < y)
-    # q_o.quick_merge_driver()
+    df_eq = df_ == df_final
+
+    print(df_.equals(df_final))
